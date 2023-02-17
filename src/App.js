62c825147c0375465ebb2cbc4a6ccb1412bc8f9f
isaacthajunior <isaacjunez669@gmail.com>
2023-02-17 11:24:38 +0100

    chore: the app started working even with changes to the package.json

--- src/App.js
+++ src/App.js
@@ -1,88 +1,88 @@
-// import MonacoEditor from 'react-monaco-editor';
-// import { useState, useEffect } from 'react';
-// import './App.css';
+// // import MonacoEditor from 'react-monaco-editor';
+// // import { useState, useEffect } from 'react';
+// // import './App.css';
 
-// const FileUploader = ({ onFileLoad }) => {
-// 	return <input type="file" onChange={(e) => onFileLoad(e.target.files[0])} />;
-// };
+// // const FileUploader = ({ onFileLoad }) => {
+// // 	return <input type="file" onChange={(e) => onFileLoad(e.target.files[0])} />;
+// // };
 
-// function App() {
-// 	const [file, setFile] = useState();
-// 	const [value, setValue] = useState();
-// 	const [theme, setTheme] = useState('vs-dark');
+// // function App() {
+// // 	const [file, setFile] = useState();
+// // 	const [value, setValue] = useState();
+// // 	const [theme, setTheme] = useState('vs-dark');
 
-// 	const editorDidMount = (editor, monaco) => {
-// 		console.log('editorDidMount', editor);
-// 		editor.focus();
-// 	};
+// // 	const editorDidMount = (editor, monaco) => {
+// // 		console.log('editorDidMount', editor);
+// // 		editor.focus();
+// // 	};
 
-// 	const editorWillMount = (monaco) => {
-// 		monaco.language.json.jsonDefaults.setDiagnosticsOptions({
-// 			validate: true,
-// 			schemas: [
-// 				{
-// 					uri: 'http://myserver/foo-schema.json',
-// 					fileMatch: ['*'],
-// 					schema: {
-// 						type: 'object',
-// 						properties: {
-// 							p1: {
-// 								enum: ['v1', 'v2'],
-// 							},
-// 							p2: {
-// 								$ref: 'http://myserver/bar-schema.json',
-// 							},
-// 						},
-// 					},
-// 				},
-// 			],
-// 		});
-// 	};
+// // 	const editorWillMount = (monaco) => {
+// // 		monaco.language.json.jsonDefaults.setDiagnosticsOptions({
+// // 			validate: true,
+// // 			schemas: [
+// // 				{
+// // 					uri: 'http://myserver/foo-schema.json',
+// // 					fileMatch: ['*'],
+// // 					schema: {
+// // 						type: 'object',
+// // 						properties: {
+// // 							p1: {
+// // 								enum: ['v1', 'v2'],
+// // 							},
+// // 							p2: {
+// // 								$ref: 'http://myserver/bar-schema.json',
+// // 							},
+// // 						},
+// // 					},
+// // 				},
+// // 			],
+// // 		});
+// // 	};
 
-// 	useEffect(() => {
-// 		if (file) {
-// 			var reader = new FileReader();
-// 			reader.onload = async (e) => {
-// 				setValue(e.target.result);
-// 			};
-// 			reader.readAsText(file);
-// 		}
-// 	}, [file]);
+// // 	useEffect(() => {
+// // 		if (file) {
+// // 			var reader = new FileReader();
+// // 			reader.onload = async (e) => {
+// // 				setValue(e.target.result);
+// // 			};
+// // 			reader.readAsText(file);
+// // 		}
+// // 	}, [file]);
 
-// 	const handleThemeChange = () => {
-// 		if (theme === 'vs-dark') {
-// 			setTheme('vs');
-// 		} else {
-// 			setTheme('vs-dark');
-// 		}
-// 	};
+// // 	const handleThemeChange = () => {
+// // 		if (theme === 'vs-dark') {
+// // 			setTheme('vs');
+// // 		} else {
+// // 			setTheme('vs-dark');
+// // 		}
+// // 	};
 
-// 	return (
-// 		<>
-// 			<FileUploader onFileLoad={setFile} />
-// 			<button
-// 				style={{
-// 					background: theme === 'vs-dark' ? 'black' : 'white',
-// 					color: theme === 'vs-dark' ? 'white' : 'black',
-// 				}}
-// 				onClick={handleThemeChange}
-// 			>
-// 				Set {theme === 'vs-dark' ? 'light' : 'dark'} theme
-// 			</button>
-// 			<MonacoEditor
-// 				height="100vh"
-// 				language="javascript"
-// 				// editorWillMount={editorWillMount}
-// 				editorWillMount={editorWillMount}
-// 				theme={theme}
-// 				value={value}
-// 				// options={{ readOnly: true, lineNumbers: 'off', minimap: { enabled: false } }}
-// 			/>
-// 		</>
-// 	);
-// }
+// // 	return (
+// // 		<>
+// // 			<FileUploader onFileLoad={setFile} />
+// // 			<button
+// // 				style={{
+// // 					background: theme === 'vs-dark' ? 'black' : 'white',
+// // 					color: theme === 'vs-dark' ? 'white' : 'black',
+// // 				}}
+// // 				onClick={handleThemeChange}
+// // 			>
+// // 				Set {theme === 'vs-dark' ? 'light' : 'dark'} theme
+// // 			</button>
+// // 			<MonacoEditor
+// // 				height="100vh"
+// // 				language="javascript"
+// // 				// editorWillMount={editorWillMount}
+// // 				editorWillMount={editorWillMount}
+// // 				theme={theme}
+// // 				value={value}
+// // 				// options={{ readOnly: true, lineNumbers: 'off', minimap: { enabled: false } }}
+// // 			/>
+// // 		</>
+// // 	);
+// // }
 
-// export default App;
+// // export default App;
 
 import React, { useState, useEffect } from 'react';
 import MonacoEditor, { MonacoDiffEditor } from 'react-monaco-editor';
@@ -107,132 +107,141 @@ const CodeEditor = () => {
 		}
 	}, [file]);
 
-	const editorWillMount = (monaco) => {
-		// monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
-		// 	noSemanticValidation: true,
-		// 	noSyntaxValidation: false
-		// });
+	// const editorWillMount = (monaco) => {
+	// monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+	// 	noSemanticValidation: true,
+	// 	noSyntaxValidation: false
+	// });
 
-		// monaco.languages.javascript.javascriptDefaults.setDiagnosticsOptions({
-		// 	noSemanticValidation: false,
-		// 	noSyntaxValidation: false,
-		// });
-		// monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
-		// 	noSemanticValidation: false,
-		// 	noSyntaxValidation: false,
-		// });
-		// validation settings
-		monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
-			noSemanticValidation: true,
-			noSyntaxValidation: false,
-		});
+	// monaco.languages.javascript.javascriptDefaults.setDiagnosticsOptions({
+	// 	noSemanticValidation: false,
+	// 	noSyntaxValidation: false,
+	// });
+	// monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+	// 	noSemanticValidation: false,
+	// 	noSyntaxValidation: false,
+	// });
+	// validation settings
+	// monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+	// 	noSemanticValidation: true,
+	// 	noSyntaxValidation: false,
+	// });
 
-		// compiler options
-		monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
-			target: monaco.languages.typescript.ScriptTarget.ES6,
-			allowNonTsExtensions: true,
-		});
+	// // compiler options
+	// monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+	// 	target: monaco.languages.typescript.ScriptTarget.ES6,
+	// 	allowNonTsExtensions: true,
+	// });
 
-		// extra libraries
-		var libSource = [
-			'declare class Facts {',
-			'    /**',
-			'     * Returns the next fact',
-			'     */',
-			'    static next():string',
-			'}',
-		].join('\n');
-		var libUri = 'ts:filename/facts.d.ts';
-		monaco.languages.typescript.javascriptDefaults.addExtraLib(
-			libSource,
-			libUri
-		);
-		// When resolving definitions and references, the editor will try to use created models.
-		// Creating a model for the library allows "peek definition/references" commands to work with the library.
-		monaco.editor.createModel(
-			libSource,
-			'typescript',
-			monaco.Uri.parse(libUri)
-		);
+	// extra libraries
+	// var libSource = [
+	// 	'declare class Facts {',
+	// 	'    /**',
+	// 	'     * Returns the next fact',
+	// 	'     */',
+	// 	'    static next():string',
+	// 	'}',
+	// ].join('\n');
+	// var libUri = 'ts:filename/facts.d.ts';
+	// monaco.languages.typescript.javascriptDefaults.addExtraLib(
+	// 	libSource,
+	// 	libUri
+	// );
+	// When resolving definitions and references, the editor will try to use created models.
+	// Creating a model for the library allows "peek definition/references" commands to work with the library.
+	// 	monaco.editor.createModel(
+	// 		libSource,
+	// 		'typescript',
+	// 		monaco.Uri.parse(libUri)
+	// 	);
 
-		var jsCode = [
-			'"use strict";',
-			'',
-			'class Chuck {',
-			'    greet() {',
-			'        return Facts.next();',
-			'    }',
-			'}',
-		].join('\n');
+	// 	var jsCode = [
+	// 		'"use strict";',
+	// 		'',
+	// 		'class Chuck {',
+	// 		'    greet() {',
+	// 		'        return Facts.next();',
+	// 		'    }',
+	// 		'}',
+	// 	].join('\n');
 
-		monaco.editor.create(document.getElementById('container'), {
-			value: jsCode,
-			language: 'javascript',
-		});
-	};
+	// 	monaco.editor.create(document.getElementById('container'), {
+	// 		value: jsCode,
+	// 		language: 'javascript',
+	// 	});
+	// };
 
-	const onChange = (newValue) => {
-		console.log('onChange', newValue);
-	};
-
-	let editor;
-	const editorDidMount = (ed) => {
-		console.log('editorDidMount', ed, ed.getValue(), ed.getModel());
-		editor = ed;
-	};
-
-	const changeEditorValue = () => {
-		if (editor) {
-			editor.setValue('// code changed! \n');
-		}
-	};
+	//getting syntax highlighting for javascript
+	// const editorWillMount = (monaco) => {
+	// 	monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+	// 		target: monaco.languages.typescript.ScriptTarget.ES6,
+	// 		allowNonTsExtensions: true,
+	// 	});
+	// };
 
 	const changeBySetState = () => {
 		setCode('// code changed by setState! \n');
 	};
 
-	const setDarkTheme = () => {
-		setTheme('vs-dark');
+	const setDarkTheme = (e) => {
+		// setTheme('vs-dark');
+		e.preventDefault();
+
+		setTheme((prev) => (prev === 'vs-dark' ? 'hc-black' : 'vs-dark'));
 	};
 
-	const setLightTheme = () => {
+	const setLightTheme = (e) => {
+		e.preventDefault();
+		
 		setTheme('vs-light');
 	};
 
 	const options = {
+		// autoIndent: 'full',
+		// contextmenu: true,
+		// fontFamily: 'monospace',
+		// fontSize: 13,
+		// lineHeight: 24,
+		// hideCursorInOverviewRuler: true,
+		// matchBrackets: 'always',
+		// minimap: {
+		// 	enabled: true,
+		// },
+		// scrollbar: {
+		// 	horizontalSliderSize: 4,
+		// 	verticalSliderSize: 18,
+		// },
+
 		selectOnLineNumbers: true,
 		roundedSelection: false,
 		readOnly: false,
 		cursorStyle: 'line',
-		automaticLayout: false,
+		automaticLayout: true,
 	};
 
 	return (
 		<div>
 			<div>
-				<button onClick={changeEditorValue} type="button">
-					Change value
-				</button>
 				<button onClick={changeBySetState} type="button">
 					Change by setState
 				</button>
 				<button onClick={setDarkTheme} type="button">
-					Set dark theme
+					Set dark theme ({theme === 'vs-dark' ? 'hc-black' : 'vs-dark'})
 				</button>
-				<button onClick={setLightTheme} type="button">
-					Set light theme
-				</button>
+				{theme !== 'vs-light' && (
+					<button onClick={setLightTheme} type="button">
+						Set light theme
+					</button>
+				)}
 				<FileUploader onFileLoad={setFile} />
 			</div>
 			<hr />
 			<MonacoEditor
 				height="400"
 				language="javascript"
-				editorWillMount={editorWillMount}
+				// editorWillMount={editorWillMount}
 				value={code}
 				options={options}
-				onChange={onChange}
-				editorDidMount={editorDidMount}
 				theme={theme}
 			/>
 		</div>
@@ -275,9 +284,68 @@ const App = () => (
 		<CodeEditor />
 		<hr />
 		<hr />
-		<h2>Another editor (showing a diff)</h2>
-		<DiffEditor />
+		{/* <h2>Another editor (showing a diff)</h2>
+		<DiffEditor /> */}
 	</div>
 );
 
 export default App;
+
+// import React from 'react';
+// import MonacoEditor from 'react-monaco-editor';
+// import * as monaco from 'monaco-editor';
+// import { MonacoServices } from 'monaco-languageclient';
+
+// const MONACO_OPTIONS = {
+// 	autoIndent: 'full',
+// 	automaticLayout: true,
+// 	contextmenu: true,
+// 	fontFamily: 'monospace',
+// 	fontSize: 13,
+// 	lineHeight: 24,
+// 	hideCursorInOverviewRuler: true,
+// 	matchBrackets: 'always',
+// 	minimap: {
+// 		enabled: false,
+// 	},
+// 	readOnly: false,
+// 	scrollbar: {
+// 		horizontalSliderSize: 4,
+// 		verticalSliderSize: 18,
+// 	},
+// };
+
+// export default function App() {
+// 	const editorDidMount = (editor) => {
+// 		MonacoServices.install(monaco);
+// 		if (editor && editor.getModel()) {
+// 			const editorModel = editor.getModel();
+// 			if (editorModel) {
+// 				editorModel.setValue('{\n    "sayHello": "hello"\n}');
+// 			}
+// 		}
+// 		editor.focus();
+// 	};
+
+// 	const onChange = (newCode, event) => {
+// 		console.log('onChange', newCode);
+// 	};
+
+// 	return (
+// 		<div>
+// 			<h1>My React App</h1>
+
+// 			<div>
+// 				<MonacoEditor
+// 					width="100%"
+// 					height="80vh"
+// 					language="json"
+// 					theme="vs"
+// 					options={MONACO_OPTIONS}
+// 					onChange={onChange}
+// 					editorDidMount={editorDidMount}
+// 				/>
+// 			</div>
+// 		</div>
+// 	);
+// }
--- src/index.js
+++ src/index.js
@@ -12,3 +12,61 @@ root.render(
 );
 
 
+// import React from 'react';
+// import MonacoEditor from 'react-monaco-editor';
+// import * as monaco from 'monaco-editor';
+// import { MonacoServices } from 'monaco-languageclient';
+
+// const MONACO_OPTIONS = {
+// 	autoIndent: 'full',
+// 	automaticLayout: true,
+// 	contextmenu: true,
+// 	fontFamily: 'monospace',
+// 	fontSize: 13,
+// 	lineHeight: 24,
+// 	hideCursorInOverviewRuler: true,
+// 	matchBrackets: 'always',
+// 	minimap: {
+// 		enabled: false,
+// 	},
+// 	readOnly: false,
+// 	scrollbar: {
+// 		horizontalSliderSize: 4,
+// 		verticalSliderSize: 18,
+// 	},
+// };
+
+// export default function App() {
+// 	const editorDidMount = (editor) => {
+// 		MonacoServices.install(monaco);
+// 		if (editor && editor.getModel()) {
+// 			const editorModel = editor.getModel();
+// 			if (editorModel) {
+// 				editorModel.setValue('{\n    "sayHello": "hello"\n}');
+// 			}
+// 		}
+// 		editor.focus();
+// 	};
+
+// 	const onChange = (newCode, event) => {
+// 		console.log('onChange', newCode);
+// 	};
+
+// 	return (
+// 		<div>
+// 			<h1>My React App</h1>
+
+// 			<div>
+// 				<MonacoEditor
+// 					width="100%"
+// 					height="80vh"
+// 					language="json"
+// 					theme="vs"
+// 					options={MONACO_OPTIONS}
+// 					onChange={onChange}
+// 					editorDidMount={editorDidMount}
+// 				/>
+// 			</div>
+// 		</div>
+// 	);
+// }
